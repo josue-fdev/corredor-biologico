bbd3a4020a5206571d7faee2f96c1f9e729dff66
josue-fdev <josuefallasm26@hotmail.com>
2022-02-18 22:05:54 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var i = document.getElementById('info');
+// var m = $('#motivo');
+var box = document.getElementById('box-text');
+i.onclick = () => {
+    box.style.display = "block";
+    i.style.display = "none";
+};
+var mark = document.getElementById('mark');
+mark.onclick = () => {
+    box.style.display = "none";
+    i.style.display = "block";
+}
+// var q = document.getElementById('sobre-nosotros');
+// q.onclick = () => {
+//     box.style.display = "none";
+//     i.style.display = "block";
+// }
+
